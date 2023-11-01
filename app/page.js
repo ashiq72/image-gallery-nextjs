c9229e86d23e60cc0f23515713959f44c589d1f6
Md ashiquzzaman <ahmedashik18k@gmail.com>
2023-11-02 01:39:55 +0600

    Drag and drop added

--- app/page.js
+++ app/page.js
@@ -1,31 +1,21 @@
 "use client";
+
+import ImageGallery from "./components/ImageGallery";
+
 import Image from "next/image";
-import ImageCard from "./components/ImageCard";
 import { useRef, useState } from "react";
-import Link from "next/link";
-
-// async function getData() {
-//   const res = await fetch("https://jsonplaceholder.typicode.com/photos");
-//   if (!res.ok) {
-//     throw new Error("Failed to fetch data");
-//   }
-
-//   return res.json();
-// }
 
 export default function Home() {
   const [images, setImages] = useState([]);
-  console.log(images);
+
+  const [selectedImages, setSelectedImages] = useState([]);
+
   const fileInputRef = useRef(null);
   function selectFiles() {
     fileInputRef.current.click();
   }
   function handleImage(e) {
     setImages(e.target.files[0]);
-  }
-  function deleteImage(index) {
-    setImages((prevImages) => prevImages.filter((_, i) => i !== index));
-    console.log(index);
   }
 
   function onFileSelect(event) {
@@ -45,35 +35,79 @@ export default function Home() {
     }
   }
 
-  // const data = await getData();
-  // console.log(data);
+  const handleCheckboxChange = (index) => {
+    if (selectedImages.includes(index)) {
+      setSelectedImages(
+        selectedImages.filter((selectedId) => selectedId !== index)
+      );
+    } else {
+      setSelectedImages([...selectedImages, index]);
+    }
+  };
+  function handleDelete() {
+    const updatedImages = images.filter((_, i) => !selectedImages.includes(i));
+    setImages(updatedImages);
+    setSelectedImages([]);
+  }
+
+  const handleDragStart = (e, index) => {
+    e.dataTransfer.setData("text/plain", index);
+  };
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+  const handleDrop = (e, toIndex) => {
+    e.preventDefault();
+    const fromIndex = e.dataTransfer.getData("text/plain");
+    const updatedImages = [...images];
+    const [draggedImage] = updatedImages.splice(fromIndex, 1);
+    updatedImages.splice(toIndex, 0, draggedImage);
+    setImages(updatedImages);
+  };
 
   return (
     <div>
       <div className="flex justify-between border-b-2 py-4 px-8">
-        <div className="flex gap-2">
-          <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
-          <h1 className="text-lg font-semibold">3 Files Selected</h1>
+        <div className="flex items-center gap-2">
+          <input
+            type="checkbox"
+            checked={selectedImages.length > 0}
+            id="vehicle1"
+            name="vehicle1"
+            value="Bike"
+          />
+          <h1 className="text-lg font-semibold">
+            {selectedImages.length} Files Selected
+          </h1>
         </div>
 
         <div>
-          <button className="text-red-700">Delete files</button>
+          <button
+            onClick={handleDelete}
+            className="bg-red-500 text-white p-2 rounded-md mt-2"
+            disabled={selectedImages.length === 0}
+          >
+            Delete Selected
+          </button>
         </div>
       </div>
       <div className="grid grid-cols-5 gap-4 p-4">
         {images?.map((image, index) => (
           <div
             key={index}
+            onDragStart={(e) => handleDragStart(e, index)}
+            onDragOver={handleDragOver}
+            onDrop={(e) => handleDrop(e, index)}
             className={`${
-              index === 0 ? "row-span-2 col-span-2" : "row-auto"
-            } col-span-1 relative`}
+              index === 0 ? "row-span-2 col-span-2" : "row-auto "
+            } col-span-1 relative border-2 border-gray-100 rounded-md`}
           >
-            <span
-              onClick={() => deleteImage(index)}
-              className="z-50 absolute right-2 text-red-600 text-2xl cursor-pointer"
-            >
-              &times;
-            </span>
+            <input
+              type="checkbox"
+              className="absolute top-2 left-2"
+              checked={selectedImages.includes(index)}
+              onChange={() => handleCheckboxChange(index)}
+            />
             <Image
               src={image.url}
               alt={index}
@@ -86,8 +120,10 @@ export default function Home() {
         <div
           onClick={() => handleImage}
           className={`${
-            images?.length <= 0 ? "row-span-2 col-span-2 " : "row-auto "
-          }  flex items-center justify-center border-dotted border-2 rounded-md border-gray-500 h-44`}
+            images?.length <= 0
+              ? "row-span-2 col-span-2 w-full h-full p-[100%] "
+              : "row-auto col-auto "
+          }col-span-1 row-span-1 bg-slate-300 flex items-center justify-center border-dotted border-2 rounded-md border-gray-500 min-content relative p-[43%] `}
         >
           <span className="text-blue-600" role="button" onClick={selectFiles}>
             {" "}
@@ -106,3 +142,25 @@ export default function Home() {
     </div>
   );
 }
+
+// const Home = () => {
+//   const images = [
+//     { id: "1", url: "/image-1.webp", isFeature: false },
+//     { id: "2", url: "/image-2.webp", isFeature: true },
+//     { id: "3", url: "/image-3.webp", isFeature: false },
+//     { id: "4", url: "/image-4.webp", isFeature: false },
+//     { id: "5", url: "/image-5.webp", isFeature: false },
+//     { id: "6", url: "/image-6.webp", isFeature: false },
+//   ];
+
+//   return (
+//     <div className="App">
+//       <h1>Image Gallery</h1>
+//       <div className="grid grid-cols-5 gap-4 p-4">
+//         <ImageGallery images={images} />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Home;
